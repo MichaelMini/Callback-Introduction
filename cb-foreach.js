e4537d7b3e6f0b0431edd80d061f1e897585554e
MichaelMini <mcho911@gmail.com>
2017-01-12 20:43:20 +0000

    Refactored forEach(). Finial

--- cb-foreach.js
+++ cb-foreach.js
@@ -1,39 +1,16 @@
 // The second argument/parameter is expected to be a function
-// function findWaldo(arr, found) {
-//   for (var i = 0; i < arr.length; i++) {
-//     if (arr[i] === "Waldo") {
-//       found();   // execute callback
-//     }
-//   }
-// }
 
-
-// function actionWhenFound() {
-//   console.log("Found him!");
-// }
-
-// findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
-
-// var array = ["Alice", "Bob", "Waldo", "Winston"];
-
-// function findEach(arr, found){
-// 	var index = 0;
-// 	index += 1;
-// 	if (arr[index] === "Waldo") {
-// 	  found();   // execute callback
-// 	}
-// }
+function findWaldo(arr, found) {
+	arr.forEach( function(entry, i) {
+		if (entry === "Waldo") {
+	    actionWhenFound(i);   // execute callback
+	  }
+	});
+}
 
 function actionWhenFound(i) {
   console.log('Found Waldo at index ' + i +'!');
 }
 
-// array.forEach(findEach(array, actionWhenFound));
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
 
-
-var arr = ["Alice", "Bob", "Waldo", "Winston"];
-arr.forEach( function(entry, i) {
-	if (entry === "Waldo") {
-    actionWhenFound(i);   // execute callback
-  }
-});
